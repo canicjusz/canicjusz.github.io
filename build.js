21a267f101e5a19f1cb33d8448dfabf4bc46a275
canicjusz <janek.bananek371@gmail.com>
2021-03-04 08:45:06 +0100

    server side rendering test

--- build.js
+++ build.js
@@ -1,4 +1,71 @@
 const fs = require('fs')
-fs.writeFile('text.txt', "xD", err=>{
-  console.log(err)
-});
+
+const MarkdownIt = require('markdown-it'),
+    md = new MarkdownIt(),
+    mdwfm = md.use(require('markdown-it-front-matter'), fm=>fm)
+
+const routes = []
+
+let navbars = ''
+let navNumber = 1;
+const source = './markdown'
+const destination = './pages'
+
+let template = fs.readFileSync('./template.html').toString()
+
+const hamburger = `<div class="hamburger"><div class="hamburger__cheese"></div><div class="hamburger__meat"></div><div class="hamburger__bun"></div></div>`
+
+const createFiles = (fromDir = source, toDir = destination) => fs.readdir(fromDir, (err, dirs) => {
+  const files = dirs.filter(directory => directory.includes('.'))
+  const folders = dirs.filter(directory => !directory.includes('.'))
+  folders.forEach(name => {
+    if (!fs.existsSync(toDir+'/'+name)) {
+      fs.mkdirSync(toDir+'/'+name); 
+    }
+    createFiles(fromDir+'/'+name, toDir+'/'+name)
+  })
+  files.forEach(name=>{
+    const string = fs.readFileSync(fromDir+'/'+name).toString()
+    const html = md.render(string)
+    const file = template.replace('<!-- content -->', html)
+    // TODO: add this custom element u kno what
+    fs.writeFileSync(toDir+'/'+name.replace('.txt', '.html'), file)
+    if(name === 'portfolio.txt'){
+      fs.writeFileSync('./index.html', file)
+    }
+  })
+})
+
+const editTemplate = () => {
+  template = template.replace('<!-- navbar -->', navbars)
+  createFiles()
+  fs.writeFileSync('./routes.json', JSON.stringify(routes))
+}
+
+const createNavbar = (dir = source) => fs.readdir(dir, (err, dirs) => {
+  const currNavNumber = navNumber
+  let navbar = currNavNumber === 1 ? `<div class="navbar-container">${hamburger}<div class="navbar-${currNavNumber} navbar" style="z-index: ${currNavNumber}; transform: translateX(-180px);"><ul class="navbar__list">` : `<div class="navbar-container"><div class="navbar-${currNavNumber} navbar" style="z-index: ${currNavNumber}; transform: translateX(-240px);">${hamburger}<ul class="navbar__list">`
+  const files = dirs.filter(directory => directory.includes('.'))
+  const folders = dirs.filter(directory => !directory.includes('.'))
+  files.forEach((name, i) => { 
+    const nameWithNewExtension = name.replace('.txt', '.html')
+    const path = dir.replace(source, destination).slice(1) + '/' + nameWithNewExtension; 
+    navbar += `<li class="navbar__element navbar__file"><a class="navbar__link" href="${path}">${name.slice(0, -4)}</a></li>`
+    // TODO: i fucked something up here
+    routes.push(path); 
+    if(i === files.length - 1 && folders.length === 0){
+      navbar += `</ul></div></div>`
+      navbars += navbar
+      editTemplate()
+    }
+  })
+  folders.forEach((name) => {
+    navNumber++
+    navbar += `<li class="navbar__element navbar__folder" to="${navNumber}">${name}<span class="navbar__traingle"></span></li>`
+    createNavbar(dir+'/'+name)
+  })
+  navbar += `</ul></div></div>`
+  navbars += navbar
+})
+
+createNavbar()
--- script.js
+++ script.js
@@ -1,166 +1,56 @@
-// https://github.com/egoist/front-matter
-const frontMatter = (str, delimiter = '---') => {
-  if (!str && str !== '') {
-    throw new TypeError('Expect str to be a string!')
-  }
-
-  str = str.trim()
-
-  const RE = new RegExp(`^${delimiter}\\n+([\\s\\S]+)\\n+${delimiter}(?:\\n([\\s\\S]*))?$`)
-
-  const splits = str.split('\n')
-  const unmatchResult = {
-    body: str
-  }
-  if (!splits[0] || splits[0] !== delimiter || !RE.test(str)) {
-    return unmatchResult
-  }
-
-  const [, head, body] = RE.exec(str)
-  return {
-    head,
-    body
-  }
-}
-
-let navNumber = 1
-const navbarsContainer = document.querySelector('.navbars-container')
-const main = document.querySelector('main')
 const getRoutes = fetch('/routes.json').then((response) => response.json())
-const md = markdownit({
-  html: true
-})
+const main = document.querySelector('main')
+const hamburgers = document.querySelectorAll('.hamburger')
+const folderLinks = document.querySelectorAll('.navbar__folder')
+const filesLinks = document.querySelectorAll('.navbar__link')
 
-getRoutes.then(({allRoutes, ...obj}) => {
-  allRoutes.forEach(({route, path}) => {
-    page(route, () => {
-      fetch(path).then(response => 
-        response.text()
-      ).then(result => {
-        const {head, body} = frontMatter(result)
-        document.title = head ? head + ' - canicjusz.github.io' : 'canicjusz.github.io'
-        main.innerHTML = md.render(body)
-        const grids = document.querySelectorAll('.grid')
-        for(let i = grids.length; i--;){
-          const element = grids[i]
-          const files = element.getAttribute('files').split(',')
-          const folder = element.getAttribute('folder')
-          const grid = createGrid(files, folder)
-          element.parentElement.replaceChild(grid, element)
-        }
-      })
+getRoutes.then((routes) => {
+  routes.forEach((route) => {
+    page(route, ()=>{
+      console.log('x')
     })
   })
-  page.redirect('/', '/pages/portfolio.html')
+  // page.redirect('/', '/pages/portfolio.html')
   page('*', ()=>{
-    fetch('/error404.txt').then(response =>
-      response.text()
-    ).then(result=>{
-      const {body} = frontMatter(result)
-      main.innerHTML = md.render(body)
-      document.title = 'No page found';
-    })
+
   })
   page.start()
-  createNavbar(obj.folders, obj.files)
 })
 
-function createGrid(files, folder){
-  const grid = document.createElement('div')
-  files.forEach(file=>{
-    const image = document.createElement('img')
-    image.src = '/static/'+folder+'/'+file
-    image.classList.add('grid__img')
-    grid.appendChild(image)
+for(let i = filesLinks.length; i--;){
+  const element = filesLinks[i]
+  element.addEventListener('click', ()=>{
+    fetch(element.href).then(response => response.text()).then(text=>{
+        const dummyDiv = document.createElement('div')
+        dummyDiv.innerHTML = text
+        const newMain = dummyDiv.querySelector('main')
+        main.innerHTML = newMain.innerHTML
+      })
   })
-  grid.classList.add('grid')
-  return grid
 }
 
-function createNavbar(folders, files) {
-  const navbar = document.createElement('div')
-  const navbarContainer = document.createElement('div')
-  navbar.classList.add('navbar-'+navNumber)
-  navbar.classList.add('navbar')
-  navbar.style.zIndex = navNumber
-  navbarContainer.classList.add('navbar-container')
-  if(navNumber === 1){
-    navbar.style.transform = 'translateX(-180px)'
-    createHamburger(navbarContainer, navbar)
-    createList(folders, files, navbar)
+for(let i = folderLinks.length; i--;){
+  const element = folderLinks[i]
+  const to = element.getAttribute('to')
+  const navbarToOpen = document.querySelector('.navbar-'+to)
+  element.addEventListener('click', ()=>{
+    navbarToOpen.classList.add('navbar--show')
+  })
+}
+
+for(let i = hamburgers.length; i--;){
+  const hamburger = hamburgers[i]
+  if(hamburger.parentElement.classList.contains('navbar')){
+    const navbar = hamburger.parentElement
+    hamburger.classList.add('hamburger--clicked')
+    hamburger.addEventListener('click', ()=>{
+      navbar.classList.remove('navbar--show')
+    })
   }else{
-    navbar.style.transform = 'translateX(-240px)'
-    createHamburger(navbar, navbar)
-    createList(folders, files, navbar)
-  }
-  navbarContainer.appendChild(navbar)
-  navbarsContainer.prepend(navbarContainer)
-}
-
-function createHamburger(parent, navbar){
-  const hamburger = document.createElement('div')
-  const bun = document.createElement('div')
-  const cheese = document.createElement('div')
-  const meat = document.createElement('div')
-  hamburger.classList.add('hamburger')
-  bun.classList.add('hamburger__bun')
-  meat.classList.add('hamburger__meat')
-  cheese.classList.add('hamburger__cheese')
-  if(navNumber === 1){
+    const navbar = hamburger.nextElementSibling
     hamburger.addEventListener('click', ()=>{
       hamburger.classList.toggle('hamburger--clicked')
       navbar.classList.toggle('navbar--show')
     })
-  }else{
-    hamburger.classList.add('hamburger--clicked')
-    hamburger.addEventListener('click', ()=>{
-      navbar.classList.toggle('navbar--show')
-    })
   }
-  hamburger.appendChild(cheese)
-  hamburger.appendChild(meat)
-  hamburger.appendChild(bun)
-  parent.appendChild(hamburger)
-}
-
-function createList(folders, files, parent) {
-  const ul = document.createElement('ul')
-  ul.classList.add('navbar__list')
-  folders.forEach(folder => {
-    navNumber++
-    createFolderElement(folder.name, ul)
-    createNavbar(folder.folders, folder.files)
-  })
-  files.forEach(file => {
-    createFileElement(file.name, file.route, ul)
-  })
-  parent.appendChild(ul)
-}
-
-function createFolderElement(name, parent) {
-  const li = document.createElement('li')
-  li.classList.add('navbar__element')
-  li.classList.add('navbar__folder')
-  const triangle = document.createElement('span')
-  triangle.classList.add('navbar__traingle')
-  li.textContent = name
-  li.appendChild(triangle)
-  const currNavNumber = navNumber
-  li.addEventListener('click', ()=>{
-    const navbarToShow = document.querySelector('.navbar-'+currNavNumber)
-    navbarToShow.classList.add('navbar--show')
-  })
-  parent.appendChild(li)
-}
-
-function createFileElement(name, route, parent) {
-  const li = document.createElement('li')
-  li.classList.add('navbar__element')
-  li.classList.add('navbar__file')
-  const anchor = document.createElement('a')
-  anchor.classList.add('navbar__link')
-  anchor.href = route
-  anchor.textContent = name
-  li.appendChild(anchor)
-  parent.appendChild(li)
 }
